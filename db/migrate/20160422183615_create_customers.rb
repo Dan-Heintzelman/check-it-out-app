b5a1c04940df33b5beed615d741bc9352e73b071
Dan Heintzelman & Michael Yao <pair+heintzdm.michaelyao88@devbootcamp.com>
2016-04-22 14:37:26 -0500

    Initial Models, Migrations, Associatons

---
+++ db/migrate/20160422183615_create_customers.rb
@@ -0,0 +1,13 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :username
+      t.string :financial_info
+      t.timestamps null: false
+    end
+  end
+end
