050531fcb7221082375fa5bb9d1bc70401c7fd49
Dan Heintzelman & Michael Yao <pair+heintzdm.michaelyao88@devbootcamp.com>
2016-04-22 18:54:15 -0500

    Add basic customer controller

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,1 +1,49 @@
-class
+class CustomerController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def new
+    #display a form that passes customer params to the create route
+  end
+
+  def create
+    @customer = Customer.new(params[:customer])
+    if @customer.save
+      session[:user_id] = @customer.id
+      redirect_to @customer
+    else
+      @errors = @customer.errors.full_messages
+      render :new
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to @user, notice: 'Your profile has been updated!' #profile view,
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :password, :email, :username)
+    end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :session, only: [:index, :new, :create]
     delete 'logout' => 'session#destroy'
 
+  resources :customers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
