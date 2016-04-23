c5e5d42a9be5939e63de4973baf72e01fcbecdc5
Zach Barton <email": null>
2016-04-23 10:57:02 -0500

    Fix test to pending

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  let :user = Customer.new(first_name: "Zach", last_name: "Barton", email: "Zach@Barton.com", password: "password", username: "zbarton", financial_info: "AMEX")
-
+  pending "add some examples to (or delete) #{__FILE__}"
 end
