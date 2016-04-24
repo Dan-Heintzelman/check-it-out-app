d5ac84b551603c6762c5aea07ec73c6db77550e0
Ben Giamarino <email": null>
2016-04-24 12:06:33 -0500

    Customer model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ActiveRecord::Base
+  validates_presence_of :username, :email, :password_digest
+  validates_uniqueness_of :email, :username
+
   has_secure_password
 
   has_many :seatings
