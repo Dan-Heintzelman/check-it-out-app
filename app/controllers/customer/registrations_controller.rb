546b592e2c6b030118f5aaa3c53318d4b0e7dddf
Dan Heintzelman <heintzelman@gmail.com>
2016-04-25 17:24:17 -0500

    remove some comments

--- app/controllers/customer/registrations_controller.rb
+++ app/controllers/customer/registrations_controller.rb
@@ -1,27 +1,5 @@
 class Customer::RegistrationsController < Devise::RegistrationsController
   respond_to :json, :html
-  # before_filter :check_params
-  #
-  # def check_params
-  #   p params
-  # end
-  #
-  # def create
-  #   @customer = Customer.new(params[:customer])
-  #   if @customer.save
-  #     # sign_in(:customer, @customer)
-  #     puts "yay"
-  #     # render json: {location: customer_path(@customer)}
-  #   else
-  #     puts "nay"
-  #     # @errors = @customer.errors.full_messages
-  #     # render json: {location: new_customer_path, status: :unprocessable_entity }
-  #   end
-  # end
-  #
-  # private
-  #   def customer_params
-  #     params.require(:customer).permit(:first_name, :last_name, :password, :email)
-  #   end
+
 
 end
