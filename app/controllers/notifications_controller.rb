401d4dd31a2835188d0a2f59debaeb33eb4d27f0
Zach Barton <email": null>
2016-04-27 18:54:19 -0500

    Text message support

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,15 +5,16 @@ respond_to :json
 
     account_sid = ENV['ACCOUNT_SID']
     auth_token = ENV['AUTH_TOKEN']
-    # text_person = params['']
-
+    text_destination = params[:phone]
+    text_amount = params[:price]
+    text_amount = ((text_amount.to_f*100).to_i)/100.00
 
     @client = Twilio::REST::Client.new account_sid, auth_token
     @client.account.messages.create({
       :from => '+15138959488',
-      :to => '+15132953508',
-      :body => 'Holy Crap, slice-it sends texts!',
-      :media_url => 'http://farm2.static.flickr.com/1075/1404618563_3ed9a44a3a.jpg',
+      :to => text_destination,
+      :body => "Hey, buddy, you owe someone $#{text_amount} so you better pay up!",
+      :media_url => 'http://farm2.static.flickr.com/1075/1404618563_3ed9a44a3a.jpg'
     })
     render nothing: true, status: 200, content_type: "json"
 
