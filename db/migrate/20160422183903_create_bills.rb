a58ca5300dcf350a9edc4b333718b334a000d963
Ben Giamarino <email": null>
2016-04-28 20:54:18 -0500

    Remove extraneous seating id from bill table - tested in console

--- db/migrate/20160422183903_create_bills.rb
+++ db/migrate/20160422183903_create_bills.rb
@@ -1,7 +1,6 @@
 class CreateBills < ActiveRecord::Migration
   def change
     create_table :bills do |t|
-      t.integer :seating_id
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20160424224248) do
   enable_extension "plpgsql"
 
   create_table "bills", force: :cascade do |t|
-    t.integer  "seating_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
